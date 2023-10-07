5d488fcca435325ef54b4b696c5f77ee29f692ed
yousseffdil <fdilyoussef@gmail.com>
2023-10-07 14:36:19 +0200

    Register users to database

--- app/src/main/java/com/example/myapplication_login/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication_login/RegisterActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterActivity : AppCompatActivity() {
@@ -12,6 +13,7 @@ class RegisterActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+        //region init
         var returnLogIn = findViewById<Button>(R.id.arrow_back)
         var registerBtn = findViewById<Button>(R.id.buttonRegister)
 
@@ -19,23 +21,44 @@ class RegisterActivity : AppCompatActivity() {
         var secondName = findViewById<EditText>(R.id.SecondNameE)
         var DNI = findViewById<EditText>(R.id.DNIEditText)
         var password = findViewById<EditText>(R.id.PasswordEditText)
+        //endregion
         returnLogIn.setOnClickListener {
             finish()
         }
-
-        registerBtn.setOnClickListener{
-            db.collection("Usuarios").document("users12").set(
+        registerBtn.setOnClickListener {
+            db.collection("Usuarios").document().set(
                 hashMapOf(
                     "DNI" to DNI.text.toString(),
                     "Name" to name.text.toString(),
                     "Password" to password.text.toString(),
-                    "SecondName" to secondName.text.toString(),
+                    "SecondName" to secondName.text.toString()
                 )
-            )
+            ).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    println("Firestore")
+                } else {
+                    val exception = task.exception
+                    if (exception != null) {
+                        println("Firestore")
+                    }
+                }
+            }
         }
     }
-
-
-
-
+    private fun isDatabaseConnected(): Boolean {
+        try {
+            db.collection("Usuarios").document("Users").get()
+            return true
+        } catch (e: Exception) {
+            return false
+        }
+    }
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error en la conexión a la base de datos")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
 }
